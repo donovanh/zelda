4682a740b93c47c5661d54cf826b16e26be6e79c
Donovan Hutchinson <don@converser.io>
2013-04-20 20:56:18 +0100

    Expanded css output

--- config.rb
+++ config.rb
@@ -1,5 +1,3 @@
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
@@ -8,7 +6,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
